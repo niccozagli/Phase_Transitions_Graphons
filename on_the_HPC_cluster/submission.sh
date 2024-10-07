8976c325fd2fc887d0a8e65395fcb6ab5d06db3f
Niccolo Zagli <niccolo.zagli@su.se>
2024-10-07 10:53:09 +0200

    Changed the submission file, added the bash line

--- on_the_HPC_cluster/submission.sh
+++ on_the_HPC_cluster/submission.sh
@@ -1,6 +1,7 @@
-#PBS -lselect=1:ncpus=2:mem=3gb                                                                                                       
-#PBS -lwalltime=24:00:00                                                                                                              
+#!/bin/bash                                                                                        
+#PBS -l select=1:ncpus=2:mem=3gb                                                                   
+#PBS -l walltime=24:00:00                                                                          
 
 module load julia/1.6.4
 cd $HOME/Graphon
-julia Integrate_Periodic_Potential.jl ./parameters.csv  $index
+#julia Integrate_Periodic_Potential.jl ./parameters.csv  $index  
